f8e2368288ff477371bf7eea046c53fcf24d5da2
Julius Csoregh <julius.csoregh@gmail.com>
2018-03-16 17:06:26 +0100

    Lagt till så att bakgrunden fadear när man trycker på menyikonen (lila)

---
+++ site/js/main.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+  var element = document.querySelector(".menu");
+  element.classList.toggle("show");
+
+  element = document.querySelector("main");
+  element.classList.toggle("fade");
+
+}
